960edd51be1d6d2cdbfb5571dff57364fa11c20b
ronalth26 <mrlayme26@gmail.com>
2024-06-06 17:42:20 -0500

    prueba feedback

--- resources/views/profile/edit_profile.blade.php
+++ resources/views/profile/edit_profile.blade.php
@@ -1,14 +1,16 @@
+
+ChatGPT
 <div id="EditProfileModal" class="modal fade" role="dialog">
     <div class="modal-dialog modal-lg">
-        <!-- Modal content-->
+        <!-- Contenido del Modal -->
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Edit Profile</h5>
-                <button type="button" aria-label="Close" class="close outline-none" data-dismiss="modal">×</button>
+                <h5 class="modal-title">Editar Perfil</h5>
+                <button type="button" aria-label="Cerrar" class="close outline-none" data-dismiss="modal">×</button>
             </div>
             <form method="POST" id="editProfileForm" enctype="multipart/form-data">
                 <div class="alert alert-info">
-                    Note: This is just UI. you need to develop Backend for update
+                    Nota: Esto es solo la interfaz de usuario. necesitas desarrollar el Backend para actualizar
                 </div>
                 <div class="modal-body">
                     <div class="alert alert-danger d-none" id="editProfileValidationErrorsBox"></div>
@@ -17,17 +19,17 @@
                     {{csrf_field()}}
                     <div class="row">
                         <div class="form-group col-sm-6">
-                            <label>Name:</label><span class="required">*</span>
+                            <label>Nombre:</label><span class="required">*</span>
                             <input type="text" name="name" id="pfName" class="form-control" required autofocus
                                    tabindex="1">
                         </div>
                         <div class="form-group col-sm-6 d-flex">
                             <div class="col-sm-4 col-md-6 pl-0 form-group">
-                                <label>Profile Image:</label>
+                                <label>Imagen de Perfil:</label>
                                 <br>
                                 <label
                                         class="image__file-upload btn btn-primary text-white"
-                                        tabindex="2"> Choose
+                                        tabindex="2"> Elegir
                                     <input type="file" name="photo" id="pfImage" class="d-none">
                                 </label>
                             </div>
@@ -46,11 +48,11 @@
                     </div>
                     <div class="text-right">
                         <button type="submit" class="btn btn-primary" id="btnPrEditSave"
-                                data-loading-text="<span class='spinner-border spinner-border-sm'></span> Processing..."
-                                tabindex="5">Save
+                                data-loading-text="<span class='spinner-border spinner-border-sm'></span> Procesando..."
+                                tabindex="5">Guardar
                         </button>
                         <button type="button" class="btn btn-light ml-1 edit-cancel-margin margin-left-5"
-                                data-dismiss="modal">Cancel
+                                data-dismiss="modal">Cancelar
                         </button>
                     </div>
                 </div>
@@ -58,4 +60,3 @@
         </div>
     </div>
 </div>
-
